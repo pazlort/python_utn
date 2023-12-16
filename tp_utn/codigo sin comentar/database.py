c806ad0ecc9aae020e425346448eab71a75e6572
pazlort <pazmarialort@gmail.com>
2023-12-16 14:30:08 -0300

    falta doc

--- tp_utn/codigo sin comentar/database.py
+++ tp_utn/codigo sin comentar/database.py
@@ -1,28 +1,16 @@
-# Importa la clase SqliteDatabase del módulo peewee
 from peewee import *
-
-# Importa la clase Mensajes del módulo mensajes
 from mensajes import Mensajes
 
-# Crea una instancia de SqliteDatabase con el nombre del archivo de base de datos
 db = SqliteDatabase("db_tp_final.db")
-# Crea una instancia de la clase Mensajes
 mensaje = Mensajes()
 
 
-# Define la clase BaseModel que actuará como clase base para todos los modelos y especifica la base de datos
 class BaseModel(Model):
-    """
-    # Clase base para todos los modelos. Establece la conexión a la base de datos.
-    """
-
     class Meta:
         database = db
 
 
-# Define el modelo Historiaclinica que hereda de BaseModel
 class Historiaclinica(BaseModel):
-    # Modelo de datos para representar la información de las historias clínicas.
     nro_historia_clinica = AutoField(unique=True)
     nombre_mascota = CharField()
     edad = IntegerField()
@@ -38,15 +26,12 @@ class Historiaclinica(BaseModel):
 
 
 try:
-    # Intenta establecer la conexión a la base de datos y crear las tablas
     db.connect()
     print("Conexión a la base de datos establecida correctamente.")
     db.create_tables([Historiaclinica])
     print("tablas creadas")
 except OperationalError as e:
-    # Si hay un error arroja un print con el mismo.
     print(f"Error al conectar a la base de datos: {e}")
 finally:
-    # Cierra la conexión a la base de datos en cualquier caso
     db.close()
     print("Conexión a la base de datos cerrada correctamente.")
