59026b8be6f8ceec12e237497d0545de6cf2cef5
pazlort <pazmarialort@gmail.com>
2023-12-16 18:19:23 -0300

    terminado

--- tp_utn/__main__.py
+++ tp_utn/__main__.py
@@ -1,14 +1,7 @@
-# Importa la clase Tk del módulo tkinter. Tk es la clase principal que representa la ventana principal de la interfaz gráfica.
 from tkinter import Tk
-
-# Importa la clase Ventana desde un módulo llamado "vista". Este módulo contiene la implementación de la interfaz gráfica.
 from vista import Ventana
 
-# Verifica si el script está siendo ejecutado directamente (no importado como un módulo).
 if __name__ == "__main__":
-    # Crea una instancia de la clase Tk, que representa la ventana principal de la interfaz gráfica. El nombre "master" es una convención común para referirse a la ventana principal.
     master = Tk()
-    # Crea una instancia de la clase Ventana (definida en el módulo "vista"), pasando la ventana principal (master) como argumento. Esto inicializa y configura la interfaz gráfica.
     Ventana(master)
-    # Inicia el bucle principal de eventos de la interfaz gráfica, lo que permite que la ventana se mantenga abierta y responda a las interacciones del usuario hasta que la ventana se cierre.
     master.mainloop()
--- tp_utn/mensajes.py
+++ tp_utn/mensajes.py
@@ -1,23 +1,11 @@
-# Importa la clase messagebox del módulo tkinter
 from tkinter import messagebox
 
 
 class Mensajes:
     def showinfo(self, mensaje):
-        # Muestra un cuadro de diálogo de información.
-        # Parameters:
-        # - mensaje (str): El mensaje que se mostrará en el cuadro de diálogo.
-        # Returns:
-        # - str: El resultado del cuadro de diálogo.
         return messagebox.showinfo(
             message=mensaje, title="Historias Clinicas Veterinarias"
         )
 
     def askquestion(self, mensaje1, mensaje2):
-        # Muestra un cuadro de diálogo de pregunta.
-        # Parameters:
-        # - mensaje1 (str): El mensaje principal de la pregunta.
-        # - mensaje2 (str): El mensaje secundario de la pregunta.
-        # Returns:
-        # - str: El resultado de la pregunta.
         return messagebox.askquestion(mensaje1, mensaje2)
